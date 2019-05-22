0a45de52e4d3f351b64aa5955788656423b892fb
rus <rru@npotelecom.ru>
2019-05-22 15:19:11 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+std::pair<std::string, int> encode(const std::string& s) {
+
+
+    return {"", 5};
+}
+
+std::string decode(const std::string& s, int n) {
+
+    return "";
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
